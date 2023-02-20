d0fb0d6561e3ead0a804b389d614c60f0742cb98
George Dombrovskyi <125083041+GeorgeDombrovskyi@users.noreply.github.com>
2023-02-20 12:39:36 +0200

    Update README.js

--- README.js
+++ README.js
@@ -1,7 +1,7 @@
 # Javascript
 
-//  Ranadom number from 1 to 100 
-    function getRndInteger(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
+// Ranadom number from 1 to 100 
+        function getRndInteger(min, max) {
+          return Math.floor(Math.random() * (max - min + 1) ) + min;
+        }
 

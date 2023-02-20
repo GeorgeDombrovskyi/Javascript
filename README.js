2396b2649be531859b568503af97551607519390
George Dombrovskyi <125083041+GeorgeDombrovskyi@users.noreply.github.com>
2023-02-20 12:50:10 +0200

    Update README.js

--- README.js
+++ README.js
@@ -5,3 +5,14 @@
           return Math.floor(Math.random() * (max - min + 1) ) + min;
         }
 
+--------------------------
+
+// Current Date
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+// getFullYear() – Provides current year like 2022.
+// getMonth() – Provides current month values 0-11. Where 0 for Jan and 11 for Dec. So added +1 to get the result.
+// getDate() – Provides day of the month values 1-31.
+
+--------------------------

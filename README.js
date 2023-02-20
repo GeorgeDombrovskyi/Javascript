eabcfe3aaca0333bd054839efc0335ca8a9e3664
George Dombrovskyi <125083041+GeorgeDombrovskyi@users.noreply.github.com>
2023-02-20 13:08:58 +0200

    Update README.js

--- README.js
+++ README.js
@@ -1,13 +1,14 @@
 # Javascript
-
+--------------------------
 // Ranadom number from 1 to 100 
+
         function getRndInteger(min, max) {
           return Math.floor(Math.random() * (max - min + 1) ) + min;
         }
 
 --------------------------
-
 // Current Date
+
         var today = new Date();
         var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
